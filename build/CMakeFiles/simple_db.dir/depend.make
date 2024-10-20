612b597698226e8c80d7c9bb15a239b38574dfa5
lalithkumar24 <lali22048.it@rmkec.ac.in>
2024-10-20 19:48:50 +0000

    separate-db-header

---
+++ build/CMakeFiles/simple_db.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for simple_db.
+# This may be replaced when dependencies are built.
---
+++ db.h
@@ -0,0 +1,74 @@
+#ifndef DB_H
+#define DB_H
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+
+#define COLUMN_USERNAME_SIZE 32
+#define COLUMN_EMAIL_SIZE 255
+#define TABLE_MAX_PAGES 100
+
+typedef struct {
+    uint32_t id;
+    char username[32];
+    char email[255];
+} Row;
+
+typedef struct {
+    uint32_t num_rows;
+    void* pages[TABLE_MAX_PAGES];
+} Table;
+
+typedef struct {
+    char* buffer;
+    size_t buffer_length;
+    ssize_t input_length;
+} InputBuffer;
+
+typedef enum { 
+    META_COMMAND_SUCCESS,
+    META_COMMAND_SHOW_ROWS, 
+    META_COMMAND_UNRECOGNIZED_COMMAND 
+} MetaCommandResult;
+
+typedef enum { 
+    STATEMENT_INSERT, 
+    STATEMENT_SELECT 
+} StatementType;
+
+typedef struct {
+    StatementType type;
+    Row row_to_insert;  // only used by insert statement
+} Statement;
+
+typedef enum { 
+    EXECUTE_SUCCESS, 
+    EXECUTE_TABLE_FULL 
+} ExecuteResult;
+
+typedef enum { 
+    PREPARE_SUCCESS, 
+    PREPARE_SYNTAX_ERROR, 
+    PREPARE_UNRECOGNIZED_STATEMENT 
+} PrepareResult;
+
+// Function declarations
+void print_row(Row* row);
+void serialize_row(Row* source, void* destination);
+void deserialize_row(void* source, Row* destination);
+void* row_slot(Table* table, uint32_t row_num);
+InputBuffer* new_input_buffer();
+void close_input_buffer(InputBuffer* input_buffer);
+Table* new_table();
+void free_table(Table* table);
+MetaCommandResult do_meta_command(InputBuffer* input_buffer, Table* table);
+PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement);
+ExecuteResult execute_insert(Statement* statement, Table* table);
+ExecuteResult execute_select(Statement* statement, Table* table);
+ExecuteResult execute_statement(Statement* statement, Table* table);
+
+#endif // DB_H
